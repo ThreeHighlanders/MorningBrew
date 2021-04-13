3518cb14ce9ee9dedde242b2636b3dc3bc4bb057
Kush Patel <kush161100@gmail.com>
2021-04-13 15:46:35 -0400

    init commit

--- app/src/main/java/com/example/morningbrew/MainActivity.java
+++ app/src/main/java/com/example/morningbrew/MainActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-//starting main activity
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-//}
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
