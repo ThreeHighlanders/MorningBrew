d5eeeacf4ea28742b8ed80dd1935ee3d092e26d1
Kush Patel <kush161100@gmail.com>
2021-04-19 20:28:41 -0400

    implemented TimePicker

--- app/src/main/java/com/example/morningbrew/SettingsActivity.java
+++ app/src/main/java/com/example/morningbrew/SettingsActivity.java
@@ -2,15 +2,47 @@ package com.example.morningbrew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class SettingsActivity extends AppCompatActivity {
+    TextView tvTimer;
+    int hour, min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        tvTimer=findViewById(R.id.tvTimer);
+        
+        tvTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog= new TimePickerDialog(
+                        SettingsActivity.this,
+                        new TimePickerDialog.OnTimeSetListener(){
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                hour=hourOfDay;
+                                min=minute;
+                                Calendar calendar=Calendar.getInstance();
+                                calendar.set(0,0,0,hour,min);
+                                tvTimer.setText(DateFormat.format("hh:mm aa",calendar));
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.updateTime(hour,min);
+                timePickerDialog.show();
+            }
+        });
+
     }
 
 
